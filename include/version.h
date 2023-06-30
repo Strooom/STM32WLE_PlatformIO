e9cf3df132f4787262aec431d05751a794d092a3
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2023-06-30 11:46:46 +0200

    improve workflow

--- include/version.h
+++
@@ -1,9 +0,0 @@
-
-    // AUTO GENERATED FILE, DO NOT EDIT
-    #ifndef VERSION
-        #define VERSION "1.0.2"
-    #endif
-    #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-06-20 14:38:31.972479"
-    #endif
-    
