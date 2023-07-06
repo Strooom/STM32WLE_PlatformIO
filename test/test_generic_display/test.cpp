30bc978f5beec3a7713c7d371e427219efd1805a
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2023-07-06 21:55:24 +0200

    test: add unit tests
    * getByteOffset()
    * getBitOffset()
    * setPixel()
    * clearPixel()

--- test/test_generic_display/test.cpp
+++ test/test_generic_display/test.cpp
@@ -38,7 +38,6 @@ void test_mirrorCoordinate() {
     uint32_t c{10};
     display::mirrorCoordinate(c, display::width);
     TEST_ASSERT_EQUAL(189, c);
-
 }
 
 void test_mirrorCoordinates() {
@@ -113,6 +112,42 @@ void test_rotateCoordinates() {
     }
 }
 
+void test_getByteOffset() {
+    TEST_ASSERT_EQUAL(0, display::getByteOffset(0, 0));
+    TEST_ASSERT_EQUAL((display::bufferSize - 1), display::getByteOffset((display::width - 1), (display::height - 1)));
+}
+
+void test_getBitOffset() {
+    TEST_ASSERT_EQUAL(0, display::getBitOffset(0));
+    TEST_ASSERT_EQUAL(7, display::getBitOffset(display::width - 1));
+}
+
+void test_setPixel() {
+    display::clearAllPixels();
+    // set the pixel in the four corners, and check the related bytes in the buffer
+    display::mirroring = displayMirroring::none;
+    display::rotation  = displayRotation::rotation0;
+    display::setPixel(0, 0);
+    display::setPixel((display::width - 1), 0);
+    display::setPixel(0, (display::height - 1));
+    display::setPixel((display::width - 1), (display::height - 1));
+    TEST_ASSERT_EQUAL(0b00000001, display::displayBuffer[0]);
+    // TODO : test all 4 corners of the display
+}
+
+void test_clearPixel() {
+    // set all pixels
+    for (uint32_t i = 0; i < display::bufferSize; i++) {
+        display::displayBuffer[i] = 0xFF;   // sets all pixels on, so we can easily test the clearPixel function
+    }
+    display::clearPixel(0, 0);
+    display::clearPixel((display::width - 1), 0);
+    display::clearPixel(0, (display::height - 1));
+    display::clearPixel((display::width - 1), (display::height - 1));
+    TEST_ASSERT_EQUAL(0b11111110, display::displayBuffer[0]);
+    // TODO : test all 4 corners of the display
+}
+
 int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_inBounds);
@@ -120,5 +155,10 @@ int main(int argc, char **argv) {
     RUN_TEST(test_mirrorCoordinate);
     RUN_TEST(test_mirrorCoordinates);
     RUN_TEST(test_rotateCoordinates);
+    RUN_TEST(test_getByteOffset);
+    RUN_TEST(test_getBitOffset);
+    RUN_TEST(test_setPixel);
+    RUN_TEST(test_clearPixel);
+
     UNITY_END();
 }
