b991af7fdb8b186e56b01c44ffba1888d6f05019
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2023-11-27 16:20:34 +0100

    testing version lib

--- lib/version/version.h
+++ lib/version/version.h
@@ -9,15 +9,9 @@ class version {
     static uint8_t getIsVersionMajor() { return isVersionMajor; }
     static uint8_t getIsVersionMinor() { return isVersionMinor; }
     static uint8_t getIsVersionPatch() { return isVersionPatch; }
-    static uint8_t getToBeVersionMajor() { return toBeVersionMajor; }
-    static uint8_t getToBeVersionMinor() { return toBeVersionMinor; }
-    static uint8_t getToBeVersionPatch() { return toBeVersionPatch; }
     static buildType getBuildType() { return theBuildType; }
     static buildEnvironment getBuildEnvironment() { return theBuildEnvironment; }
     static const char* getIsVersionAsString() { return isVersionString; }
-    static const char* getToBeVersionAsString() { return toBeVersionString; }
-
-    static bool isVersionEqualsToBeVersion();
 
     static void setIsVersion();
     
@@ -34,14 +28,9 @@ class version {
     static uint8_t isVersionMinor;
     static uint8_t isVersionPatch;
 
-    static uint8_t toBeVersionMajor;
-    static uint8_t toBeVersionMinor;
-    static uint8_t toBeVersionPatch;
-
     static buildType theBuildType;
     static buildEnvironment theBuildEnvironment;
 
     static constexpr uint8_t maxVersionStringLength{40U};        // maximum length of string holding the version, eg V30_50_00_00-100-g7da1beb-PROD
     static char isVersionString[maxVersionStringLength];
-    static char toBeVersionString[maxVersionStringLength];
 };
--- test/test_generic_version/test.cpp
+++ test/test_generic_version/test.cpp
@@ -12,13 +12,9 @@ void test_initialize() {
     TEST_ASSERT_EQUAL_UINT8(0, version::isVersionMajor);
     TEST_ASSERT_EQUAL_UINT8(0, version::isVersionMinor);
     TEST_ASSERT_EQUAL_UINT8(0, version::isVersionPatch);
-    TEST_ASSERT_EQUAL_UINT8(0, version::toBeVersionMajor);
-    TEST_ASSERT_EQUAL_UINT8(0, version::toBeVersionMinor);
-    TEST_ASSERT_EQUAL_UINT8(0, version::toBeVersionPatch);
     TEST_ASSERT_EQUAL(buildType::unknown, version::theBuildType);
     TEST_ASSERT_EQUAL(buildEnvironment::unknown, version::theBuildEnvironment);
     TEST_ASSERT_EQUAL_STRING("", version::isVersionString);
-    TEST_ASSERT_EQUAL_STRING("", version::toBeVersionString);
 
     version::setIsVersion();
     // After initialization values...
@@ -28,82 +24,23 @@ void test_initialize() {
     TEST_ASSERT_EQUAL(buildInfo::theBuildType, version::theBuildType);
     TEST_ASSERT_EQUAL(buildInfo::theBuildEnvironment, version::theBuildEnvironment);
     TEST_ASSERT_GREATER_THAN(0, strlen(version::getIsVersionAsString()));
-
-    version::setToBeVersionFromString("v1.2.3");
-    TEST_ASSERT_GREATER_THAN(0, strlen(version::getToBeVersionAsString()));
 }
 
-void test_isVersionEqualsToBeVersion() {
-    version::isVersionMajor = 1;
-    version::isVersionMinor = 2;
-    version::isVersionPatch = 3;
+void test_toString() {
+    // For test coverage only
+    TEST_ASSERT_EQUAL_STRING("unknown", toString(buildType::unknown));
+    TEST_ASSERT_EQUAL_STRING("development", toString(buildType::development));
+    TEST_ASSERT_EQUAL_STRING("production", toString(buildType::production));
+    TEST_ASSERT_EQUAL_STRING("integrationTest", toString(buildType::integrationTest));
 
-    version::toBeVersionMajor = 1;
-    version::toBeVersionMinor = 2;
-    version::toBeVersionPatch = 3;
-    TEST_ASSERT_TRUE(version::isVersionEqualsToBeVersion());
-
-    version::toBeVersionMajor = 4;
-    version::toBeVersionMinor = 2;
-    version::toBeVersionPatch = 3;
-    TEST_ASSERT_FALSE(version::isVersionEqualsToBeVersion());
-
-    version::toBeVersionMajor = 1;
-    version::toBeVersionMinor = 4;
-    version::toBeVersionPatch = 3;
-    TEST_ASSERT_FALSE(version::isVersionEqualsToBeVersion());
-
-    version::toBeVersionMajor = 1;
-    version::toBeVersionMinor = 2;
-    version::toBeVersionPatch = 4;
-    TEST_ASSERT_FALSE(version::isVersionEqualsToBeVersion());
-}
-
-void test_isValidToBeVersionString() {
-    TEST_ASSERT_FALSE(version::isValidToBeVersionString(nullptr));        // nullptr
-    TEST_ASSERT_FALSE(version::isValidToBeVersionString(""));             // empty string
-    char testString[version::maxVersionStringLength];
-    memset(testString, 'A', version::maxVersionStringLength);
-    memcpy(testString, "v1.2.3", 6U);
-    TEST_ASSERT_FALSE(version::isValidToBeVersionString(testString));        // too long
-
-    char testString2[version::maxVersionStringLength] = "v1.2.3";
-    TEST_ASSERT_TRUE(version::isValidToBeVersionString(testString2));        // valid
-}
-
-void test_setToBeVersionFromString() {
-    version::setToBeVersionFromString("v6_7_8");
-    TEST_ASSERT_EQUAL_UINT8(6, version::toBeVersionMajor);
-    TEST_ASSERT_EQUAL_UINT8(7, version::toBeVersionMinor);
-    TEST_ASSERT_EQUAL_UINT8(8, version::toBeVersionPatch);
-    TEST_ASSERT_EQUAL_STRING("v6.7.8", version::toBeVersionString);
-}
-
-void test_replaceDelimitorWithDot() {
-    char testString[] = "V30_50_00_00-100-g7da1beb-PROD";
-    version::replaceDelimitorWithDot(testString, '_');
-    TEST_ASSERT_EQUAL_STRING("V30.50.00.00-100-g7da1beb-PROD", testString);
-}
-
-void test_stripLeadingV() {
-    char testString1[] = "V30_50_00_00-100-g7da1beb-PROD";
-    version::stripLeadingV(testString1);
-    TEST_ASSERT_EQUAL_STRING("30_50_00_00-100-g7da1beb-PROD", testString1);
-    char testString2[] = "v30_50_00_00-100-g7da1beb-PROD";
-    version::stripLeadingV(testString2);
-    TEST_ASSERT_EQUAL_STRING("30_50_00_00-100-g7da1beb-PROD", testString2);
-    char testString3[] = "v30_50_00_00-100-g7da1beb-PROD";
-    version::stripLeadingV(testString3);
-    TEST_ASSERT_EQUAL_STRING("30_50_00_00-100-g7da1beb-PROD", testString3);
+    TEST_ASSERT_EQUAL_STRING("unknown", toString(buildEnvironment::unknown));
+    TEST_ASSERT_EQUAL_STRING("local", toString(buildEnvironment::local));
+    TEST_ASSERT_EQUAL_STRING("ci", toString(buildEnvironment::ci));
 }
 
 int main(int argc, char** argv) {
     UNITY_BEGIN();
     RUN_TEST(test_initialize);
-    RUN_TEST(test_isVersionEqualsToBeVersion);
-    RUN_TEST(test_isValidToBeVersionString);
-    RUN_TEST(test_setToBeVersionFromString);
-    RUN_TEST(test_replaceDelimitorWithDot);
-    RUN_TEST(test_stripLeadingV);
+    RUN_TEST(test_toString);
     UNITY_END();
 }
